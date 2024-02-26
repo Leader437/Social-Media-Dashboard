ebc3775e295c5ecc120f8f3037a317b1de17bddd
Saif <raosaif786pak@gmail.com>
2024-02-27 01:31:05 +0500

    -

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let body = document.querySelector('body');
 let toggle = document.querySelector('input');
 
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        body.classList.add('dark');
+        toggle.checked = true;
+}
+
 toggle.addEventListener('change', e => {
         body.classList.toggle('dark');
 })
@@ -9,7 +14,3 @@ toggle.addEventListener('change', e => {
 //         const newColorScheme = event.matches ? "dark" : "light";
 //         console.log(newColorScheme);
 // });
-
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        toggle.checked = true;
-}
