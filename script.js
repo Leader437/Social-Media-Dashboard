d265d5df21dd1ca22e0eeb1489c98ab92314a041
Saif <raosaif786pak@gmail.com>
2024-02-27 01:32:10 +0500

    -

--- script.js
+++ script.js
@@ -9,8 +9,3 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
 toggle.addEventListener('change', e => {
         body.classList.toggle('dark');
 })
-
-// window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-//         const newColorScheme = event.matches ? "dark" : "light";
-//         console.log(newColorScheme);
-// });
