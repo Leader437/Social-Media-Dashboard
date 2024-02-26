23d93721b031d75bb7a892b1faef48bbb7d0d67f
Saif <raosaif786pak@gmail.com>
2024-02-27 01:13:49 +0500

    almost completed

---
+++ script.js
@@ -0,0 +1,15 @@
+let body = document.querySelector('body');
+let toggle = document.querySelector('input');
+
+toggle.addEventListener('change', e => {
+        body.classList.toggle('dark');
+})
+
+// window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+//         const newColorScheme = event.matches ? "dark" : "light";
+//         console.log(newColorScheme);
+// });
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        console.log("hello");
+}
